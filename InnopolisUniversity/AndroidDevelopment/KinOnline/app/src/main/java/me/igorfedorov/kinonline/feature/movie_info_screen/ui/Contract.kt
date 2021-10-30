908cd2f1e419e369a7b784c9b8eb3f33b62e2fdc
Igor Fedorov <72979646+igorfedorovme@users.noreply.github.com>
2021-10-30 03:58:33 +0300

    - Implemented Exploding FAB, looks weird :)

--- InnopolisUniversity/AndroidDevelopment/KinOnline/app/src/main/java/me/igorfedorov/kinonline/feature/movie_info_screen/ui/Contract.kt
+++ InnopolisUniversity/AndroidDevelopment/KinOnline/app/src/main/java/me/igorfedorov/kinonline/feature/movie_info_screen/ui/Contract.kt
@@ -1,9 +1,15 @@
 package me.igorfedorov.kinonline.feature.movie_info_screen.ui
 
+import android.view.animation.Animation
 import me.igorfedorov.kinonline.base.base_view_model.Event
 
-class ViewState()
+data class ViewState(
+    val animation: Animation?
+) {
+    val isAnimationViewVisible = animation != null
+}
 
 sealed class UIEvent() : Event {
-    data class OnPlayButtonCLick(val movieUrl: String) : UIEvent()
+    data class OnPlayButtonCLick(val animation: Animation?) : UIEvent()
+    data class NavigateToVideoPlayerScreen(val movieUrl: String) : UIEvent()
 }
--- InnopolisUniversity/AndroidDevelopment/KinOnline/app/src/main/java/me/igorfedorov/kinonline/feature/movie_info_screen/ui/MovieInfoFragment.kt
+++ InnopolisUniversity/AndroidDevelopment/KinOnline/app/src/main/java/me/igorfedorov/kinonline/feature/movie_info_screen/ui/MovieInfoFragment.kt
@@ -2,10 +2,14 @@ package me.igorfedorov.kinonline.feature.movie_info_screen.ui
 
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.AnimationUtils
 import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import me.igorfedorov.kinonline.R
+import me.igorfedorov.kinonline.base.cicerone_navigation.utils.startAnimation
 import me.igorfedorov.kinonline.base.utils.loadImage
 import me.igorfedorov.kinonline.base.utils.setThrottledClickListener
 import me.igorfedorov.kinonline.databinding.FragmentMovieInfoBinding
@@ -33,13 +37,42 @@ class MovieInfoFragment : Fragment(R.layout.fragment_movie_info) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val animation =
+            AnimationUtils.loadAnimation(requireContext(), R.anim.circle_explosion_anim).apply {
+                duration = 500
+                interpolator = AccelerateDecelerateInterpolator()
+            }
+
         binding.apply {
             moviePosterImageView.loadImage(movie.posterUrl)
             titleTextView.text = movie.title
             descriptionTextView.text = movie.overview
             playMovieFab.setThrottledClickListener {
-                viewModel.processUiEvent(UIEvent.OnPlayButtonCLick(movie.video))
+                viewModel.processUiEvent(UIEvent.OnPlayButtonCLick(animation = animation))
+            }
+        }
+
+        viewModel.viewState.observe(viewLifecycleOwner, ::render)
+    }
+
+    private fun render(viewState: ViewState) {
+        navigateWithAnimation(viewState)
+        manageViewVisibility(viewState)
+    }
+
+    private fun manageViewVisibility(viewState: ViewState) {
+        binding.circle.apply {
+            isVisible = viewState.isAnimationViewVisible
+            elevation = 20F
+        }
+    }
+
+    private fun navigateWithAnimation(viewState: ViewState) {
+        viewState.animation?.let {
+            binding.circle.startAnimation(it) {
+                viewModel.processUiEvent(UIEvent.NavigateToVideoPlayerScreen(movieUrl = movie.video))
             }
         }
     }
+
 }
